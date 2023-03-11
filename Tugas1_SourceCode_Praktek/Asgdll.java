b99040c0cd958a2b06dfd775072ae1c832303085
dwiadiningsih <127500971+dwiadiningsih@users.noreply.github.com>
2023-03-11 20:01:16 +0800

    Create Asgdll.java

---
+++ Tugas1_SourceCode_Praktek/Asgdll.java
@@ -0,0 +1,16 @@
+*/Nama : Dwi Adiningsih
+Nim : 13020210021
+Hari/tgl : Jumat, 10 Maret 2022
+waktu : 7.35
+
+public class Asgdll {
+
+public static void main(String[] args) {
+	
+float f= 20.0f;
+double fll;
+	
+	fll=10.0f;
+System.out.println ("f : "+f+ "\nf11: "+fll);
+	}
+}
